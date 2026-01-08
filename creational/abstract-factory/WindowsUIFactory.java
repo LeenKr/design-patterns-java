8b3b765cfdf0a4ca6ed7fd134968b77801444f40
leen krayem <leenkkrayem@gmail.com>
2026-01-08 23:35:00 +0200

    Implement WindowsUIFactory for UI component creation

---
+++ creational/abstract-factory/WindowsUIFactory.java
@@ -0,0 +1,12 @@
+public class WindowsUIFactory implements UIFactory {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+}
