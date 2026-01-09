f9ac2bbd4034e7e46f203747f97ddde2e0b4cd9c
leen krayem <leenkkrayem@gmail.com>
2026-01-09 12:22:17 +0200

    Implement PayPalAdapter for payment processing

---
+++ structural/adapter/PayPalAdapter.java
@@ -0,0 +1,13 @@
+public class PayPalAdapter implements PaymentProcessor {
+
+    private final PayPalService payPalService;
+
+    public PayPalAdapter(PayPalService payPalService) {
+        this.payPalService = payPalService;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        payPalService.makePayment(amount);
+    }
+}
