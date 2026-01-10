c20ffbd31ad03c8d1e6db2774b1fbe966079f3a4
leen krayem <leenkkrayem@gmail.com>
2026-01-10 13:04:54 +0200

    Create NotificationDecorator.java

---
+++ structural/decorator/NotificationDecorator.java
@@ -0,0 +1,13 @@
+public abstract class NotificationDecorator implements Notification {
+
+    protected Notification wrappedNotification;
+
+    public NotificationDecorator(Notification notification) {
+        this.wrappedNotification = notification;
+    }
+
+    @Override
+    public void send(String message) {
+        wrappedNotification.send(message);
+    }
+}
